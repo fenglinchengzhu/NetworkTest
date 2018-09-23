2ee0cb72da79bc9cc15e962fffda6b57bbe2fe93
fenglinchengzhu <fenglinchengzhu@163.com>
2018-09-23 11:35:04 +0800

    XmlPullParse

--- app/src/main/java/com/example/kinglu/networktest/MainActivity.java
+++ app/src/main/java/com/example/kinglu/networktest/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.kinglu.networktest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.IOException;
+import java.io.StringReader;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -37,18 +43,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void run() {
                 try{
-                    OkHttpClient client  = new OkHttpClient();
-                    Request      request = new Request.Builder().url("https://www.baidu.com")
-                            .build();
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder().url("http://10.0.2.2/get_data2.xml").build();
                     Response response     = client.newCall(request).execute();
                     String   responseData = response.body().string();
-                    sendResponse(responseData);
+                    parseXMLWithPull(responseData);
+                   // sendResponse(responseData);
                 }catch (IOException e){
                     e.printStackTrace();
                 }
             }
         }).start();
     }
+    private void parseXMLWithPull(String xmlData){
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (eventType){
+                    case XmlPullParser.START_TAG: {
+                        if ("id".equals(nodeName)) {
+                            id = xmlPullParser.nextText();
+                        } else if ("name".equals(nodeName)) {
+                            name = xmlPullParser.nextText();
+                        } else if ("version".equals(nodeName)) {
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    case XmlPullParser.END_TAG: {
+                        if ("app".equals(nodeName)){
+                            Log.d("MainActivity", "id is : " + id);
+                            Log.d("MainActivity", "name is : " + name);
+                            Log.d("MainActivity", "version is : " + version);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     private void sendResponse(final String response){
         runOnUiThread(new Runnable() {
             @Override
